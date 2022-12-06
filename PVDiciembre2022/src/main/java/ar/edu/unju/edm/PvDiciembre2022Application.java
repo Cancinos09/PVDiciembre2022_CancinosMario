25a45b3918a7c5e78fdef4808cdc8b87d2a9f67a
mario <mario@LAPTOP-H95KPG9F>
2022-12-06 16:19:27 -0300

    Usuario: Cancinos09 - Descripcion: Se asocia el proyeto al repositorio

---
+++ PVDiciembre2022/src/main/java/ar/edu/unju/edm/PvDiciembre2022Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvDiciembre2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvDiciembre2022Application.class, args);
+	}
+
+}
